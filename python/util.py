d4c906e2a6dd2220984b90c1b5e5cef04e413179
obsproth <obsproth@users.noreply.github.com>
2017-05-03 09:20:03 +0900

    Fix UnionFind

--- python/util.py
+++ python/util.py
@@ -17,15 +17,22 @@ def factorial_mod(x, m):
 
 class UnionFind():
     def __init__(self, N):
-        self.root = [None] * N
+        self.N = N
+        self.root = list(range(N))
     def get_root(self, a):
         r = self.root[a]
-        if r is None:
-            return a
-        else:
+        if r != a:
             r = self.get_root(r)
             self.root[a] = r
-            return r
+        return r
     def connect(self, a, b):
+        if a == b:
+            raise RuntimeError(a, b)
         self.root[self.get_root(a)] = self.get_root(b)
+    def count_union(self):
+        t = 0
+        for i in range(self.N):
+            if i == self.get_root(i):
+                t += 1
+        return t
 
