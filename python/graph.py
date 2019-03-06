3054f209a6c084e86ebd46686a7b1edc6ee018fe
obsproth <obsproth@users.noreply.github.com>
2019-03-06 23:24:19 +0900

    Add UnionFindEx

--- python/graph.py
+++ python/graph.py
@@ -1,21 +1,23 @@
-class UnionFind():
-    def __init__(self, N):
-        self.N = N
-        self.root = list(range(N))
-    def get_root(self, a):
-        r = self.root[a]
-        if r != a:
-            r = self.get_root(r)
-            self.root[a] = r
-        return r
-    def connect(self, a, b):
-        if a == b:
-            raise RuntimeError(a, b)
-        self.root[self.get_root(a)] = self.get_root(b)
-    def count_union(self):
-        t = 0
-        for i in range(self.N):
-            if i == self.root[i]:
-                t += 1
-        return t
-
+class UnionFindEx:
+    def __init__(self, size):
+        #正なら根の番号、負ならグループサイズ
+        self.roots = [-1] * size
+    def getRootID(self, i):
+        r = self.roots[i]
+        if r < 0:   #負なら根
+            return i
+        else:
+            r = self.getRootID(r)
+            self.roots[i] = r
+            return r
+    def getGroupSize(self, i):
+        return -self.roots[self.getRootID(i)]
+    def connect(self, i, j):
+        r1, r2 = self.getRootID(i), self.getRootID(j)
+        if r1 == r2:
+            return False
+        if self.getGroupSize(r1) < self.getGroupSize(r2):
+            r1, r2 = r2, r1
+        self.roots[r1] += self.roots[r2]    #サイズ更新
+        self.roots[r2] = r1
+        return True
