7a3d2b2b8cbe26c1801cb0939897c0e179e7b034
obsproth <obsproth@users.noreply.github.com>
2020-05-05 22:59:28 +0900

    Update and rename D.py to ABC166_D.py

--- AtCoder/ABC166/ABC166_D.py
+++ AtCoder/ABC166/ABC166_D.py
@@ -17,3 +17,23 @@ def main():
 
 main()
 
+'''
+無限ループによる実装
+Aを0, -1, 1, -2, 2, -3, 3 ... のように動かす
+
+from itertools import count
+
+def main():
+    X = int(input())
+    for i in count():
+        for A in (-i, i):
+            for B in range(-i, i+1):
+                if A**5 - B**5 == X:
+                    print(A, B)
+                    return
+
+
+main()
+'''
+
+
